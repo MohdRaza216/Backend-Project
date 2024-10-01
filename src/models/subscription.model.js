4ff4072ef93c42b342d1e35986b0bbcc86811b22
MohdRaza216 <mohdrazamoghul2024@gmail.com>
2024-10-01 15:48:03 +0530

    adding getUserChannelProfile API

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 const subscriptionSchema = new Schema({
-    Subscriber: {
+    subscriber: {
         type: Schema.Types.ObjectId, // user who is subscribing 
         ref: "User"
     },
