a7f40b7bc7af7ba9050df41b998c61b3d0faa63d
MohdRaza216 <mohdrazamoghul2024@gmail.com>
2024-10-04 10:30:11 +0530

    Fix logout funtionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { ObjectId } from "bson";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
@@ -112,8 +113,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined
+      $unset: {
+        refreshToken: 1 // this removes the field from document
       }
     },
     {
@@ -306,10 +307,10 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         isSubscribed: {
           $cond: {
             if: {
-              $in: [req.user?._id, "$subscribers.subscriber"],
-              then: true,
-              else: false
-            }
+              $in: [req.user._id, "$subscribers.subscriber"] // Dynamically referencing the user's ObjectId
+            },
+            then: true,
+            else: false
           }
         }
       }
@@ -331,56 +332,60 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   if (!channel?.length) {
     throw new ApiError(404, "Channel does not exists")
   }
-  
+
   return res.status(200).json(new ApiResponse(200, channel[0], "User channel fetched successfully"))
 })
-  const getWatchHistory = asyncHandler(async(req, res) => {
-    const user = await User.aggregate([
-      {
-        $match: {
-          _id: new mongoose.Types.ObjectId.createFromHexString(req.user._id)
-        }
-      },
-      {
-        $lookup: {
-          from: "videos",
-          localField: "watchHistory",
-          foreignField: "_id",
-          as: "watchHistory",
-          pipeline: [
-            {
-              $lookup: {
-                from: "users",
-                localField: "owner",
-                foreignField: "_id",
-                as: "owner",
-                pipeline: [
-                  {
-                    $project: {
-                      username: 1,
-                      fullname: 1,
-                      avatar: 1
-                    }
-                  },
-                  {
-                    $addFields: {
-                      owner: {
-                        $first: "$owner"
-                      }
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: req.user._id
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    fullname: 1,
+                    avatar: 1
+                  }
+                },
+                {
+                  $addFields: {
+                    owner: {
+                      $first: "$owner"
                     }
                   }
-                ]
+                }
+              ]
 
-              }
             }
-          ]
-        }
+          }
+        ]
       }
+    }
 
-    ])
+  ])
 
-    return res.status(200).json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"))
-  })
+  if (user && user.length > 0 && user[0].watchHistory) {
+    return res.status(200).json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"));
+  } else {
+    return res.status(404).json(new ApiResponse(404, {}, "Watch history not found"));
+  }
+})
 
 
 
